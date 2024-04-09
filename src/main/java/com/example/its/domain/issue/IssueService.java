7ddd2c909712e8764110284ce693fba3d02a3d01
Masaki-Ohara <masakingu921@gmail.com>
2024-04-09 14:07:59 +0900

    データベースからデータ取得

--- src/main/java/com/example/its/domain/issue/IssueService.java
+++ src/main/java/com/example/its/domain/issue/IssueService.java
@@ -1,2 +1,16 @@
-package com.example.its.domain.issue;public class IssueService {
+package com.example.its.domain.issue;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class IssueService {
+    private final IssueRepository issueRepository;
+
+    public List<IssueEntity> findAll() {
+        return issueRepository.findAll();
+    }
 }
